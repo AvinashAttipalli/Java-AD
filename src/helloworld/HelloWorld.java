b1c25b56504997e7829b085fb8f4f8f40457c665
Avinash <attipalli.avinash@gmail.com>
2016-09-08 00:27:33 -0400

    Sample- Initial files

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+package helloworld; // java package should be in lower case
+
+//java class name should follow the camel case
+public class HelloWorld {
+
+//java is a case sensitive see the "main" method for instance
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world!"); // shortcut - "sysout + enter "
+	}
+
+}
+
+
+// "String[] args", arrays of string
+
+//main method should always be public 
+
+/* comments are of 3 type 1. single line comments and 
+2. multi-line comments 3. documentation comments     */
+
+//see next program Temperature.java in temperature package
